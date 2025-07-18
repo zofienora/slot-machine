4fc19502b9f02371b7bbae09a247e58f75feddad
Zofia Nora <sophienora92@gmail.com>
2025-07-18 09:44:15 +0200

    added Machine component for css art slot machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Machine from './components/Machine';
 
 function App() {
 
   return (
     <>
-   
+      <Machine />
     </>
   )
 }
---
+++ src/components/Machine.jsx
@@ -0,0 +1,11 @@
+function Machine() {
+  return (
+    <div className="machine">
+      <h1>Slot Machine</h1>
+      <p>Welcome to the Slot Machine game!</p>
+      {/* Additional components and logic for the slot machine can be added here */}
+    </div>
+  );
+}
+
+export default Machine;
