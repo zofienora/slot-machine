b5bf4eeaf3d2fef4edb1eb499df9506a3895b3a3
Zofia Nora <sophienora92@gmail.com>
2025-07-18 10:14:08 +0200

    deleted to default function again for Machine function

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -1,9 +1,8 @@
 function Machine() {
   return (
-    <div className="machine">
-      <h1>Slot Machine</h1>
-      <p>Welcome to the Slot Machine game!</p>
-      {/* Additional components and logic for the slot machine can be added here */}
+    <div className="container">
+        
+     
     </div>
   );
 }
