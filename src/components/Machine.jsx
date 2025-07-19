16a7d80870e2dfd1113ca69185c6efae5de1d6d6
Zofia Nora <sophienora92@gmail.com>
2025-07-19 16:56:33 +0200

    more styles

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -2,13 +2,13 @@ function Machine() {
   return (
     <div className="container">
         <div className="slot-container">
-            <p className="fruit"></p>
+            <p className="fruit">?</p>
         </div>
         <div className="slot-container">
-            <p className="fruit"></p>
+            <p className="fruit">?</p>
         </div>
         <div className="slot-container">
-            <p className="fruit"></p>
+            <p className="fruit">?</p>
         </div>
      
     </div>
